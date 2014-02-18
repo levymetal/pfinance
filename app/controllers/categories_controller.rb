77604123c0cfba6e8a531a5c80c5ceb7aed04465
Christian Varga <levymetal@gmail.com>
2014-02-18 21:54:24 +1100

    only show root categories as parents on new category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
-    @categories = current_user.categories
+    @categories = current_user.categories.where(ancestry: nil)
   end
 
   # GET /categories/1/edit
