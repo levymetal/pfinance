a5151846b3df267b11a2440a5f6f499c36b89c76
levymetal <noreply@levymetal.net>
2014-02-28 21:15:54 +1100

    add time_zone to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_timezone 
+
+  def set_timezone
+    Time.zone = current_user.time_zone 
+  end
 end
