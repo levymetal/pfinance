9a8a7c905e3d8d2b55798a926699ba30c87183b3
Christian Varga <levymetal@gmail.com>
2014-05-18 10:07:12 +1000

    show parent category name in list

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,8 @@ class Category < ActiveRecord::Base
 
   default_scope { order('name asc') } 
   scope :recent, -> { joins("LEFT JOIN entries ON entries.category_id = categories.id").group('categories.id').reorder('count(entries.category_id) desc') }
+
+  def with_root
+    self.parent ? name + ' [' + parent.name + ']' : name
+  end
 end
