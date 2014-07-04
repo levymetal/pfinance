16f9f32c8caeb2917940d47874a4a75c07a32937
levymetal <noreply@levymetal.net>
2014-07-04 22:07:35 +0200

    random commented code that i'm not using.. just committing it.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveRecord::Base
   has_ancestry
+  # belongs_to :ancestry
 
   has_many :entries
 
+  # default_scope includes(:ancestry)
   default_scope { order('name asc') } 
   scope :recent, -> { joins("LEFT JOIN entries ON entries.category_id = categories.id").group('categories.id').reorder('count(entries.category_id) desc') }
 
